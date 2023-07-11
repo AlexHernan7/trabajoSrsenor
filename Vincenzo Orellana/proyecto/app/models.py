9633c545fc7ed7e58c96dbd51c09f285df0c5c0b
NicoDraven <nicolasdraven91@gmail.com>
2023-07-11 16:32:18 -0400

    Avance

--- Vincenzo Orellana/proyecto/app/models.py
+++ Vincenzo Orellana/proyecto/app/models.py
@@ -5,4 +5,7 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.TextField(max_length=200)
     precio = models.IntegerField()
-    
+    imagen = models.ImageField(upload_to="productos", null=True)
+
+    def __str__(self):
+        return self.nombre
--- Vincenzo Orellana/proyecto/app/urls.py
+++ Vincenzo Orellana/proyecto/app/urls.py
@@ -3,9 +3,9 @@ from .views import index, api, form, mision, noticias
 
 urlpatterns = [
     path('', index, name="index"),
-    path('/form', form, name="form"),
-    path('/mision', mision, name="mision"),
-    path('/api', api, name="api"),
-    path('/noticias', noticias, name="noticias"),
+    path('form', form, name="form"),
+    path('mision', mision, name="mision"),
+    path('api', api, name="api"),
+    path('noticias', noticias, name="noticias"),
 
 ]
